01fad285836d318f523affd08ee0129d590193e0
Mateo <mateodominguez3@hotmail.com>
2026-07-27 11:57:22 -0300

    clase 12

---
+++ clase_12/script.js
@@ -0,0 +1,11 @@
+
+/* console.log("hola mundo") */
+
+let password = prompt("ingrese su contraseña")
+
+if(password == "password_123"){
+    console.log("inicio de sesion exitoso")
+}
+else{
+    console.log("credenciales invalidas, vuelva a intentar")
+}
