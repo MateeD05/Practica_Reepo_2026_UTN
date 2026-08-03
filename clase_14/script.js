0b6f99c32669ccfb087de3b05aa3d69fac01ed86
Mateo <mateodominguez3@hotmail.com>
2026-08-03 13:04:53 -0300

    clase 14

---
+++ clase_14/script.js
@@ -0,0 +1,14 @@
+/* let eleccion = prompt("ingrese su personaje (pan o queso)") */
+
+/* function clacularIva (precio){
+    let iva = precio * 0.21
+    console.log("el iva de $" + precio + " es $" + iva)
+}
+
+clacularIva(4500) */
+
+function hola (nombre){
+    console.log("Hola " + nombre)
+}
+
+hola("juan")
