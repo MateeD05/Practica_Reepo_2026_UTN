6a7d7f0d8fee9bb7a38fea0757e01847c3d4dc20
Mateo <mateodominguez3@hotmail.com>
2026-08-06 20:15:08 -0300

    clase 15

---
+++ clase_15/script.js
@@ -0,0 +1,29 @@
+/* function calcular(a, b, operation){
+    let resultado
+    if(operation === "+")
+        resultado = (a + b)
+    else if(operation === "-")
+        resultado = (a - b)
+    return resultado
+} */
+
+
+
+/* let numeros = [500, 400, 100]
+let sumatoria = 0
+for (
+    let indice = 0;
+    indice < numeros.length;
+    indice = indice + 1
+) {
+    sumatoria = sumatoria + numeros[indice];
+}
+console.log("Resultado final: " + sumatoria); */
+
+
+
+let tipos_iva = [
+    ["general", 21],
+    ["reducido", 10.5],
+    ["aumentado", 27]
+]
