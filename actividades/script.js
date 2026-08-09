725f972068e964cd0ae629361f8553c2e3450b08
Mateo <mateodominguez3@hotmail.com>
2026-08-09 15:38:16 -0300

    actividades1

---
+++ actividades/script.js
@@ -0,0 +1,110 @@
+// EJERCICIO #1
+
+/* for(
+    let i = 1;
+    i <= 10;
+    i = i + 1
+){
+    console.log(i)
+} */
+
+
+// EJERCICIO #2
+
+/* for(
+    let i = 2;
+    i <= 20;
+    i = i + 2
+){
+    console.log(i)
+} */
+
+
+// EJERCICIO #3
+
+/* for(
+    let i = 1;
+    i <= 19;
+    i = i + 2
+){
+    console.log(i)
+} */
+
+
+// EJERCICIO #4
+
+/* for(
+    let i = 10;
+    i >= 1;
+    i = i - 1
+){
+    console.log(i)
+} */
+
+
+// EJERCICIO #5
+
+/* for(
+    let i = 1;
+    i <= 10;
+    i = i + 1
+){
+    console.log(i)
+    if(i === 5){
+        break
+    }
+} */
+
+
+// EJERCICIO #6
+
+/* for(
+    let i = 1;
+    i <= 10;
+    i = i + 1
+){
+    if(i === 5){
+        continue
+    }
+    console.log(i)
+} */
+
+
+// EJERCICIO #7
+
+/* function decirNombre(nombre){
+    alert("Hola " + nombre)    
+}
+
+decirNombre("Juan") */
+
+
+// EJERCICIO #8
+
+/* function saludar(nombre){
+    return("hola " + nombre)
+}
+
+alert(saludar("Mateo")) */
+
+
+// EJERCICIO #9
+
+/* function sumar(a, b){
+    return("El resultado de la suma es: " + (a + b))
+}
+
+console.log(sumar(10, 20)) */
+
+
+// EJERCICIO #10
+
+/* function restar(a, b){
+    return("El resultado de la resta es: " + (a - b))
+}
+
+console.log(restar(50, 20)) */
+
+
+// EJERCICIO #11
+
